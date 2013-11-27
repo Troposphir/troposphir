d1437f914127f629529768a4093bcaed7189f859
kevson <kevson@users.noreply.github.com>
2013-11-26 23:36:10 -1000

    Moved files around. Also updated index.php

--- index.php
+++ index.php
@@ -16,31 +16,32 @@
 	You should have received a copy of the GNU Affero General Public License 
 	along with this program.  If not, see <http://www.gnu.org/licenses/>.    
 ==============================================================================*/
-
 //error_reporting(0);
 require('configs.php');
-require('utils.php');
-require("./scripts/CRequest.php");
-require("./scripts/CDatabase.php");
+require('./include/utils.php');
+require("./include/CRequest.php");
+require("./include/CDatabase.php");
 header('X-Powered-By: Troposphir Beta');
 header('Cache-Control: no-cache, must-revalidate');
 header('Expires: Mon, 01 Jan 1996 00:00:00 GMT');
 header('Content-type: application/json');
-
+			
 if(isset($_REQUEST["json"])) {
 	$json = get_magic_quotes_gpc() ? json_decode(stripslashes($_REQUEST['json']), true) : json_decode($_REQUEST['json'], true);
-	if (!isset($json['header']) ||
-		!isset($json['body']) ||
-		!isset($json['body']['_t'])) return;
+	if (!isset($json['header'])) return;
+	if (!isset($json['body'])) return;
+	if (!isset($json['body']['_t'])) return;
 
-	$reqtype = str_replace(chr(0), '', basename($json["body"]["_t"]));
-	if (substr($reqtype, 0, 1) == "C") return; //do not acess Class files
+	//To take into consideration: whitelisting?
+	$reqtype = preg_replace("/[^a-zA-Z]/", "", basename($json['body']['_t']));
 	if (!file_exists("./scripts/$reqtype.php")) return;
-	
-	define("INCLUDE_SCRIPT", TRUE);
+
+	define("INCLUDE_SCRIPT",  TRUE);
 	require("./scripts/$reqtype.php");
-	$request = new $reqtype($config);
-	$request->work($json);
-	$request->send();
+	if (class_exists($reqtype)) {
+		$request = new $reqtype($config);
+		$request->work($json);
+		$request->send();
+	}
 }
 ?>
