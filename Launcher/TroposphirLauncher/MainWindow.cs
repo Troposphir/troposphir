13e54065b3de191b0f54c595466cb6f7610464a6
Kroltan <kroltan1@gmail.com>
2014-01-06 11:16:28 -0200

    * Glob.cs: Simple utility class to list all files in a directory.
    * MainWindow.cs: added pseudo-update checking, not complete
    
    * TroposphirLauncher.csproj:
    * TroposphirLauncher.MainWindow.cs:

--- Launcher/TroposphirLauncher/MainWindow.cs
+++ Launcher/TroposphirLauncher/MainWindow.cs
@@ -1,5 +1,8 @@
-using System.Threading.Tasks;
+using System.Security.Cryptography;
+using System.Collections.Generic;
 using System.Diagnostics;
+using System.Threading;
+using System.Linq;
 using System.Net;
 using System.IO;
 using System;
@@ -12,7 +15,10 @@ namespace TroposphirLauncher {
 			Build();
 			settingsWindow = new SettingsWindow();
 			LoadNews();
-			progressBar.Fraction = 0.1;
+			if (settingsWindow.AutoUpdate) {
+				Thread updater = new Thread(new ThreadStart(CheckUpdates));
+				updater.Start();
+			}
 		}
 
 		protected void OnDeleteEvent (object sender, DeleteEventArgs a) {
@@ -40,22 +46,70 @@ namespace TroposphirLauncher {
 			}
 		}
 
+		string ServerRequest(string path) {
+			string url = System.IO.Path.Combine(settingsWindow.TroposphirServerPath, path);
+			WebRequest request = WebRequest.Create(url);
+			Stream data = request.GetResponse().GetResponseStream();
+			data.ReadTimeout = 3000;
+			using (StreamReader reader = new StreamReader(data)) {
+				return reader.ReadToEnd();
+			}
+		}
+
 		void LoadNews() {
 			string result = string.Empty;
 			try {
-				string path = System.IO.Path.Combine(settingsWindow.TroposphirServerPath, "updateNews.php");
-				WebRequest request = WebRequest.Create(path);
-				Stream data = request.GetResponse().GetResponseStream();
-				data.ReadTimeout = 3000;
-				using (StreamReader reader = new StreamReader(data)) {
-					result = reader.ReadToEnd();
-				}
-			} catch {
-				Debug.WriteLine("Could not connect to update news");
+				result = ServerRequest("updateNews.php");
+			} catch (Exception e) {
+				Debug.WriteLine("Could not connect to update news: "+e.Message);
 				result = "Could not load update list";
 			}
 			updateTextView.Buffer.Clear();
 			updateTextView.Buffer.InsertAtCursor(result);
+		}
+
+		void CheckUpdates() {
+			Dictionary<string, string> fileHashes = new Dictionary<string, string>();
+			using (MD5 md5 = MD5.Create()) {
+				progressBar.Text = "Verifying game files";
+				List<string> files = Glob.ListAllFiles(settingsWindow.AtmosphirExecutableFolder);
+				for (int index = 0, length = files.Count; index < length; index++) {
+					string entry = System.IO.Path.Combine(settingsWindow.AtmosphirExecutableFolder, files[index]);
+					using (FileStream stream = File.OpenRead(entry)) {
+						fileHashes.Add(entry.Replace("\\", "/"), BitConverter.ToString(md5.ComputeHash(stream)));
+						progressBar.Fraction = index / (float)(length-1);
+					}
+				}
+//				//Computer-Specific dump of the game hashes for internal server testing
+//				string ss = "";
+//				foreach (string key in fileHashes.Keys) {
+//					string uri = Uri.UnescapeDataString(new Uri(new Uri(settingsWindow.AtmosphirExecutableFolder), new Uri(key)).AbsolutePath);
+//					ss += uri + "===" + fileHashes[key] + "\r\n";
+//				}
+//				File.WriteAllText("C:/xampp/htdocs/Troposphir/Server/clientHashList.txt", ss);
+			}
+			string result = string.Empty;
+			progressBar.Text = "Loading update list";
+			progressBar.Fraction = 0f;
+			try {
+				result = ServerRequest("clientHashList.txt");
+				List<string> mustUpdate = new List<string>();
+				string[] lines = result.Split("\n".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+				for (int index = 0, length = lines.Length; index < length; index++) {
+					string[] parts = lines[index].Split("===".ToCharArray(), StringSplitOptions.RemoveEmptyEntries);
+					if (parts.Length == 2) {
+						string key = System.IO.Path.Combine(settingsWindow.AtmosphirExecutableFolder, parts[0]);
+						if (fileHashes[key] != parts[1]) {
+							mustUpdate.Add(parts[0]);
+						}
+					}
+					progressBar.Fraction = index / (float)(length-1);
+				}
+				foreach (string item in mustUpdate) Debug.WriteLine(item);
+			} catch (Exception e) {
+				Debug.WriteLine("Could not connect to update server: "+e.Message);
+				progressBar.Text = "Could not connect";
+			}
 		}
 	}
 }
