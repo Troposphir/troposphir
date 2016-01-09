8d83c8c74b88b41fc4cb1c0717dc6eecedea84b7
Pedro Lopez <pedroslopez@me.com>
2016-01-09 03:54:27 -0400

    Users now have individual "Owned Items" lists.

--- Server/scripts/findItemSetsReq.php
+++ Server/scripts/findItemSetsReq.php
@@ -13,25 +13,25 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Affero General Public License for more details.
 
-  You should have received a copy of the GNU Affero General Public License 
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.    
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ==============================================================================*/
 
 ///armor = 20
 //none = 0
 //perk = 10
-//INCOMPLETE	
+//INCOMPLETE
 if (!defined("INCLUDE_SCRIPT")) return;
 class findItemSetsReq extends RequestResponse {
 	public function work($json) {
 		$fres = array();
-		
+
 		$itemSet = array();
 		$itemSet["items"]     = array(1);
 		$itemSet["id"]        = 142;
 		$itemSet["name"]      = "Guest Item Set";
 		$itemSet["created"]   = 12412412;
-		
+
 		$props = array();
 		$props['shown']   = 'true';
 		$props['is.free'] = 'true';
@@ -43,7 +43,7 @@ class findItemSetsReq extends RequestResponse {
 		$props['genders'] = '0,1';
 		$props['setCategory'] = '20';
 		$itemSet['props'] = $props;
-	
+
 		$this->addBody("fres", array("results" => array($itemSet)));
 	}
 }
