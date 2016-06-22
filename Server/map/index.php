5c184cdb973a4c6b8d5635e6b04fd35cd2b22944
Pedro Lopez <pedroslopez@me.com>
2016-06-21 20:09:53 -0400

    Some changes I had laying around...

--- Server/map/index.php
+++ Server/map/index.php
@@ -13,8 +13,8 @@
   MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
   GNU Affero General Public License for more details.
 
-  You should have received a copy of the GNU Affero General Public License 
-  along with this program.  If not, see <http://www.gnu.org/licenses/>.    
+  You should have received a copy of the GNU Affero General Public License
+  along with this program.  If not, see <http://www.gnu.org/licenses/>.
 ==============================================================================*/
 error_reporting(0);
 require("../configs.php");
@@ -24,23 +24,23 @@ $lid = $_REQUEST['lid']; //map id
 $id  = $_REQUEST['id'];  //asset id
 if (isset($lid) && is_numeric($lid) &&
 	isset($id) && is_numeric($id))
-{ 
+{
 	//Cross check request with map data
-	$db = new Database($config['driver'], $config['host'], $config['dbname'], $config['user'], $config['password']);	
-	$stmt = $db->prepare("SELECT dataId FROM " . $config['table_map'] . " 
+	$db = new Database($config['driver'], $config['host'], $config['dbname'], $config['user'], $config['password']);
+	$stmt = $db->prepare("SELECT dataId FROM " . $config['table_map'] . "
 		WHERE `id`=:id
 		AND `dataId`=:dataId");
 	$stmt->bindParam(':id', $lid, PDO::PARAM_INT);
 	$stmt->bindParam(':dataId', $id, PDO::PARAM_INT);
 	$stmt->execute();
-	
+
 	//Invalid lid and id pair
 	if ($stmt == false || $db->getRowCount($stmt) <= 0) {
 		die();
 	}
-	
+
 	//Retrieve map file
-	$stmt = $db->prepare("SELECT fileName FROM " . $config['table_assets'] . " 
+	$stmt = $db->prepare("SELECT fileName FROM " . $config['table_assets'] . "
 		WHERE `id`=:id");
 	$stmt->bindParam(':id', $id, PDO::PARAM_INT);
 	$stmt->execute();
@@ -49,9 +49,10 @@ if (isset($lid) && is_numeric($lid) &&
 	if ($stmt == false || $db->getRowCount($stmt) <= 0) {
 		die();
 	}
+
 	$row = $stmt->fetch();
 	$file = $row['fileName'];
-	
+
 	if (file_exists("./$file")) {
 		echo file_get_contents("./$file");
 	}
